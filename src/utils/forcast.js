d02c793a21effbd219f5bdbea2d05449b65e17b9
Mohammad Maaz <maaz.md@gmail.com>
2020-05-03 18:08:03 +0530

    Add about text and humity information

--- src/utils/forcast.js
+++ src/utils/forcast.js
@@ -10,7 +10,7 @@ url=`http://api.weatherstack.com/current?access_key=c31aec0c7f5a78594c3f28a464d0
 
        else{ 
         callback(undefined, {
-            forcast : `${body.current.weather_descriptions}. It is currently ${body.current.temperature} degrees out but it feels like it is ${body.current.feelslike} degree out`
+            forcast : `${body.current.weather_descriptions}. It is currently ${body.current.temperature} degrees out but it feels like it is ${body.current.feelslike} degree out. The humidity is ${body.current.humidity}`
         
  
         })}
